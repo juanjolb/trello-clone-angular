ff6935b6539d48f08163de86871154397fc95c50
juanjolb <lbarcelo.juanjo@gmail.com>
2024-04-10 16:01:34 +0200

    add github profile

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { GithubIconComponent } from '../Icons/github-icon/github-icon.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [GithubIconComponent],
   template: `
     <header>
-      <nav class="flex justify-between p-5 bg-slate-800 text-lg font-bold">
-        <h1>Trello clone</h1>
+      <nav class="flex justify-between p-5 bg-slate-800 text-lg font-bold ">
         <p>Juanjo Lozano</p>
+        <h1 class="text-2xl text-sky-100">Trello clone</h1>
+        <a
+          href="https://github.com/juanjolb"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <app-github-icon class="w-8 h-8" />
+        </a>
       </nav>
     </header>
   `,
